13af5bf40fdf8a6537eec2604d0ed0533f95b18f
smilejakdu <ash982416@gmail.com>
2021-02-24 18:22:00 +0900

    master

---
+++ sj/1weeks/두개뽑아서더하기.py
@@ -0,0 +1,19 @@
+'''
+numbers	result
+[2,1,3,4,1]	[2,3,4,5,6,7]
+[5,0,2,7]	[2,5,7,9,12]
+'''
+
+#def solution(numbers):
+#    answer = []
+#    [answer.append(numbers[number1] + numbers[number2]) for number1 in range(0, len(numbers)) for number2 in
+#     range(number1, len(numbers)) if number1 != number2 and numbers[number1] + numbers[number2] not in answer]
+#    return sorted(answer)
+
+numbers = [2,1,3,4,1]
+
+def solution(numbers):
+    return
+
+print(solution(numbers))
+
