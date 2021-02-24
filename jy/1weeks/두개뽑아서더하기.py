e57376926b7a790031d57ffd48dc7b692f698de9
junyoung <jy940608@naver.com>
2021-02-24 20:16:31 +0900

    jy

---
+++ jy/1weeks/두개뽑아서더하기.py
@@ -0,0 +1,21 @@
+from itertools import combinations
+
+def solution1(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            answer.append(numbers[i] + numbers[j])
+    return sorted(list(set(answer)))
+
+
+def solution2(numbers):
+    answer = []
+    l = list(combinations(numbers, 2))
+    for i in l:
+        answer.append(i[0]+i[1])
+    return sorted(list(set(answer)))
+
+numbers = [5, 0, 7, 9]
+
+print(solution1(numbers))
+print(solution2(numbers))
