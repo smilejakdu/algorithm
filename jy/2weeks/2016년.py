a727c814147c51208b70608fea97d2d1cb9824f1
junyoung <jy940608@naver.com>
2021-03-06 22:18:51 +0900

    2016년

--- jy/2weeks/2016년.py
+++ jy/2weeks/2016년.py
@@ -1,1 +1,11 @@
-print(11234)
+def solution1(a, b):
+    answer = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
+    month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    day = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    result = 0
+
+    for i in range(a - 1):
+        result += day[month[i] - 1]
+    result += b
+
+    return answer[(result % 7) - 1]
