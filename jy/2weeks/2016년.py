89f4d9edd2edaa3af00bf6f59225304988efb003
junyoung <jy940608@naver.com>
2021-03-08 19:30:49 +0900

    datetime 풀이

--- jy/2weeks/2016년.py
+++ jy/2weeks/2016년.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def solution1(a, b):
     answer = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
     month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
@@ -9,3 +12,15 @@ def solution1(a, b):
     result += b
 
     return answer[(result % 7) - 1]
+
+
+def solution2(a, b):
+    days = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    return days[datetime.date(2016, a, b).weekday()]
+
+
+a = 5
+b = 14
+
+print(solution1(a, b))
+print(solution2(a, b))
